aa9d32e3acf1f8b21785e33284f7fe4d5fea1ef5
William <williamhangouts@gmail.com>
2023-02-09 23:28:13 +0000

    beginning to set up practice form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import React from "react";
+import PracticeFormBackdrop from './components/PracticeFormBackdrop'
 
 function App() {
   return (
     <div>
       <h1> Practice Tracker </h1>
-
+      <PracticeFormBackdrop />
     </div>
   );
 }
