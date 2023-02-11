e5a0bc690873ddf2f07e3c1b4293539767e95072
itswillum <williamhangouts@gmail.com>
2023-02-10 23:29:21 -0500

    testing modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import React from "react";
-import PracticeFormBackdrop from './components/PracticeFormBackdrop'
+import PracticeFormBackdrop from "./components/PracticeFormBackdrop";
+import PracticeFormModal from "./components/PracticeFormModal";
 
 function App() {
   return (
     <div>
       <h1> Practice Tracker </h1>
       <PracticeFormBackdrop />
+      <PracticeFormModal />
     </div>
   );
 }
--- src/components/PracticeFormModal.js
+++ src/components/PracticeFormModal.js
@@ -1,13 +1,20 @@
-function PracticeForm() {
-    function formOpen(){
-        console.log("clicked the add button")
-    }
-    return(
-        <div>
-        Enter how much youve practiced.
-            <button className='btn' onClick={formOpen}>Add practice</button>
-        </div>
-    );
+import React from "react";
+function PracticeFormModal() {
+  console.log("Currently rendering PracticeFormModal")
+  //function formOpen() {
+  //  console.log("clicked the add button");
+  //}
+  //<button className='btn' onClick={formOpen}>
+  //Add practice
+  //</button>
+  return (
+    <div className='modal'>
+      <p>Practice Information</p>
+
+      <button className='btn btn--alt'>Cancel</button>
+      <button className='btn'>Confirm</button>
+    </div>
+  );
 }
 
-export default PracticeForm();
+export default PracticeFormModal();
