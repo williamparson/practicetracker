f671398befc64d943f6f4d0aa1ed3db003b5ce32
itswillum <45744989+itswillum@users.noreply.github.com>
2023-02-22 19:14:13 +0000

    starting to style table

--- src/components/TableEntry.js
+++ src/components/TableEntry.js
@@ -2,10 +2,9 @@ import React from "react";
 function TableEntry(props) {
   return (
     <div>
-      <h1>{props.month}</h1>
-      <h1>{props.date}</h1>
-      <h1>{props.duration}</h1>
-      <h1>{props.description}</h1>
+      <h2>
+        {props.month}/{props.date} {props.duration} {props.description}
+      </h2>
     </div>
   );
 }
