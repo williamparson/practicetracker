705c281e39fae5933fd8db40bde5ed3a537e7254
itswillum <45744989+itswillum@users.noreply.github.com>
2023-03-06 19:34:59 +0000

    Updates

---
+++ precache-manifest.e46d5a959cd8aa2553971e24f409463a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd00e5331ff593496a14",
+    "url": "/practicetracker/static/css/main.95f0d997.chunk.css"
+  },
+  {
+    "revision": "cd00e5331ff593496a14",
+    "url": "/practicetracker/static/js/main.cd00e533.chunk.js"
+  },
+  {
+    "revision": "e365fac8f9a138c9a99b",
+    "url": "/practicetracker/static/js/1.e365fac8.chunk.js"
+  },
+  {
+    "revision": "f6c6ce25aefd57e577e2",
+    "url": "/practicetracker/static/css/2.4f350fa0.chunk.css"
+  },
+  {
+    "revision": "f6c6ce25aefd57e577e2",
+    "url": "/practicetracker/static/js/2.f6c6ce25.chunk.js"
+  },
+  {
+    "revision": "960b83ff1d472196c715",
+    "url": "/practicetracker/static/js/runtime~main.960b83ff.js"
+  },
+  {
+    "revision": "0682354805e5e4a0d24083a9725f87ab",
+    "url": "/practicetracker/index.html"
+  }
+];
