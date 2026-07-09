2fd03f59db5ca5dd2814d25393cf70432c2ebff2
William Parson <williamwparson@gmail.com>
2026-07-09 13:12:06 +1000

    Updates

---
+++ precache-manifest.5c01e940180af2fa51a4ed14698056ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef14b2c2d9c7f7363670",
+    "url": "/practicetracker/static/css/main.462b608a.chunk.css"
+  },
+  {
+    "revision": "ef14b2c2d9c7f7363670",
+    "url": "/practicetracker/static/js/main.ef14b2c2.chunk.js"
+  },
+  {
+    "revision": "315327e41e41ef1b3c48",
+    "url": "/practicetracker/static/js/1.315327e4.chunk.js"
+  },
+  {
+    "revision": "4be6589e761ad1d1ef47",
+    "url": "/practicetracker/static/css/2.b0cbe913.chunk.css"
+  },
+  {
+    "revision": "4be6589e761ad1d1ef47",
+    "url": "/practicetracker/static/js/2.4be6589e.chunk.js"
+  },
+  {
+    "revision": "d68dc2a1a210b26e49d8",
+    "url": "/practicetracker/static/js/runtime~main.d68dc2a1.js"
+  },
+  {
+    "revision": "6350ebbd8e5b684dba5fa310c86d6c42",
+    "url": "/practicetracker/index.html"
+  }
+];
